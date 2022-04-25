b54065c6e42a2cefdbdae99e822495b39d445397
eonsinde <olasinde.eon@gmail.com>
2022-04-25 15:44:48 +0100

    Implemented Stack Navigation

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';  
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+
 import { Provider } from 'react-redux';
-import HomeScreen from './screen/HomeScreen';
 import store from './store';
+
+import HomeScreen from './screen/HomeScreen';
+import MapScreen from './screen/MapScreen';
+import EatsScreen from './screen/EatsScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <HomeScreen />
-      </View>
-    </Provider>
+      <Provider store={store}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <StatusBar style="auto" />
+            <Stack.Navigator>
+              <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+              <Stack.Screen name="MapScreen" component={MapScreen} options={{ headerShown: false }} />
+              <Stack.Screen name="EatsScreen" component={EatsScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
