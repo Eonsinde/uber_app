323553a844b2a1570f0d183f379bc0a36fcc7b6c
eonsinde <olasinde.eon@gmail.com>
2022-04-25 15:45:34 +0100

    Modified: Updated UI

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -3,9 +3,10 @@ import React from 'react'
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 
+
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image 
           style={{
