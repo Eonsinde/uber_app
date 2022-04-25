c1b6616d9110f268520902b3981be5a5953570dc
eonsinde <olasinde.eon@gmail.com>
2022-04-25 15:45:02 +0100

    Created FlatList Rendering

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
+import { Icon } from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
 
 
 const data = [
@@ -18,11 +21,35 @@ const data = [
 ]
 
 const NavOptions = () => {
-  return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
-  )
+    const navigation = useNavigation();
+
+    return (
+        <FlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            horizontal
+            renderItem={({item}) => (
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate(`${item.screen}`)}
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                >
+                    <View>
+                        <Image
+                            style={{ height:120, width:120, resizeMode: 'contain' }}
+                            source={{ uri: item.image }}
+                        />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`} 
+                            name="arrowright" 
+                            color="white" 
+                            type="antdesign"
+                        />
+                    </View>
+                </TouchableOpacity>
+            )}
+        />
+    )
 }
 
 export default NavOptions
