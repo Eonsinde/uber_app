425e6a6afad0b3d0d6ca23d37c5b3feee809175b
eonsinde <olasinde.eon@gmail.com>
2022-10-14 22:46:36 +0100

    Added googleplaceautocomplete form

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,13 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { StyleSheet, View, SafeAreaView, Image } from 'react-native'
 import tw from 'twrnc';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import NavOptions from '../components/NavOptions';
+import { GOOGLE_MAPS_API_KEY } from '@env';
+import { setOrigin, setDestination } from '../slice/navSlice';
 
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
+        {/* Uber logo */}
         <Image 
           style={{
             width: 100,
@@ -18,7 +25,37 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
-        <NavOptions />
+        <GooglePlacesAutocomplete 
+          placeholder='Where From?'
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'en' 
+          }}
+          onPress={(data, details=null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }))
+
+            dispatch(setDestination(null))
+          }}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={2}
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+        {/* Links to Map & Eat screen */}
+        <NavOptions /> 
       </View>
     </SafeAreaView>
   )
