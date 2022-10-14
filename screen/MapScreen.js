107f2ab06e1e9d7ff3c3f0c36c7c1f295bd6c673
eonsinde <olasinde.eon@gmail.com>
2022-10-14 22:46:12 +0100

    Rendered Map component

--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import tw from 'twrnc';
+import MapView from 'react-native-maps';
+import Map from '../components/Map';
+
+
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      {/* <Text>MapScreen</Text> */}
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}></View>
+    </SafeAreaView>
   )
 }
 
