59e4423cf9528232126ecc5ca5adc8fd99cf08dc
eonsinde <olasinde.eon@gmail.com>
2022-10-18 22:06:25 +0100

    created the ride options card component

---
+++ components/RideOptionsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+
+const RideOptionsCard = () => {
+  return (
+    <View>
+        <Text>RideOptionsCard</Text>
+    </View>
+  )
+}
+
+export default RideOptionsCard
