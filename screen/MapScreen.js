fe4b38edf4b5189379babf279943eb7f32a9516b
eonsinde <olasinde.eon@gmail.com>
2022-10-18 22:07:51 +0100

    implemented map rendering and stack navigator

--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw from 'twrnc';
-import MapView from 'react-native-maps';
+// components import
 import Map from '../components/Map';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
+import ScreenHeader from '../components/ScreenHeader';
 
 
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      {/* <Text>MapScreen</Text> */}
       <View style={tw`h-1/2`}>
         <Map />
       </View>
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator
+          screenOptions={{
+            header : ({ ...props }) => <ScreenHeader {...props} />
+          }}
+        >
+          <Stack.Screen 
+            name='NavigateCard' 
+            component={NavigateCard}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen 
+            name='RideOptionsCard' 
+            component={RideOptionsCard}
+            options={{
+              headerTitle: 'Ride Options'
+              // headerShown: false 
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </SafeAreaView>
   )
 }
