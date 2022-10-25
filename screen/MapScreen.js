d0492adcb116e9e6aeb578c3db38de610919c60c
eonsinde <olasinde.eon@gmail.com>
2022-10-25 21:03:30 +0100

    added menu button to goBack to HomeScreen

--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView, View } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import tw from 'twrnc';
+// icons import
+import { Icon } from 'react-native-elements';
 // components import
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
-import ScreenHeader from '../components/ScreenHeader';
+// import ScreenHeader from '../components/ScreenHeader';
 
 
 
-const MapScreen = () => {
+const MapScreen = ({ navigation }) => {
   const Stack = createNativeStackNavigator();
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`h-1/2`}>
+        <TouchableOpacity
+          style={tw`bg-gray-100 border-2 border-neutral-200 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+          onPress={() => navigation.goBack()}
+        >
+          <Icon 
+            name='menu'
+          />
+        </TouchableOpacity>
         <Map />
       </View>
       <View style={tw`h-1/2`}>
