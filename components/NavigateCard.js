16970f723318ae8efdfd6b59a2db10b2a77e992e
eonsinde <olasinde.eon@gmail.com>
2022-10-25 15:56:22 +0100

    prevented further navigation without set destin...

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,24 +1,26 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // googleplacesautocomplete import
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_API_KEY } from '@env';
 import tw from 'twrnc'
-import { setDestination } from '../slice/navSlice';
+import { selectDestination, setDestination } from '../slice/navSlice';
 import NavFavourites from './NavFavourites';
+import { Icon } from 'react-native-elements';
 
 
 const NavigateCard = () => {
+    const destination = useSelector(selectDestination)
     const dispatch = useDispatch()
     const navigation = useNavigation()
 
     return (
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Good Morning, Eonsinde</Text>
-            <View style={tw`border-t border-gray-200 flex-shrink`}>
+            <View style={tw`flex-shrink`}>
                 <View>
                     <GooglePlacesAutocomplete 
                         placeholder='Where To?'
@@ -43,8 +45,41 @@ const NavigateCard = () => {
                         debounce={400}
                     />
                 </View>
-                <View>
-                    <NavFavourites />
+
+                <NavFavourites />
+                <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
+                    <TouchableOpacity 
+                        onPress={() => navigation.navigate('RideOptionsCard')}
+                        disabled={!destination}
+                    >
+                        <View
+                            style={tw`flex flex-row justify-between bg-black w-24 px-4 py-3 rounded-full ${!destination && 'opacity-50'}`}
+                        >
+                            <Icon 
+                                name='car'
+                                type='font-awesome'
+                                color='white'
+                                size={16}
+                            />
+                            <Text style={tw`text-white text-center`}>Rides</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        disabled={!destination}
+                    >
+                        <View
+                            style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full ${!destination && 'opacity-50'}`}
+                        >
+                            <Icon 
+                                name='fast-food-outline'
+                                type='ionicon'
+                                color='black'
+                                size={16}
+                            />
+                            <Text style={tw`text-black text-center`}>Eats</Text>
+                        </View>
+                        
+                    </TouchableOpacity>
                 </View>
             </View>
         </SafeAreaView>
